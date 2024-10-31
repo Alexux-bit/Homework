93a94be34957fc47ab9df6d9d79c80827e120f80
Alexux-bit <alexstv128@gmail.com>
2024-10-31 22:17:30 +0200

    3rd_commit

---
+++ H1/HomeWork_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LibrarySystem;
+
+namespace LibrarySystemApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("1984", "George Orwell");
+
+
+            Book book2 = new Book("The Hobbit", "J.R.R. Tolkien", 310);
+
+            book1.CheckOut();
+            book2.ReturnBook();
+
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine(book2.ToString());
+        }
+    }
+}
